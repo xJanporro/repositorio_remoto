0051d02a9dadf2f768414a91d4419cec9aa5e329
xJanporro <janpierstein@gmail.com>
2022-03-18 14:52:44 -0400

    commit master ajax.js

---
+++ ajax.js
@@ -0,0 +1,20 @@
+	
+	$(document).on('click','#btn-add',function(e) {
+		var data = $("#user_form").serialize();
+		$.ajax({
+			data: data,
+			type: "post",
+			url: "backend/save.php",
+			success: function(dataResult){
+					var dataResult = JSON.parse(dataResult);
+					if(dataResult.statusCode==200){
+						$('#addEmployeeModal').modal('hide');
+						alert('Data added successfully !'); 
+                        location.reload();						
+					}
+					else if(dataResult.statusCode==201){
+					   alert(dataResult);
+					}
+			}
+		});
+	});
